e69f2d636675e4eb71c58a2833d39be2bb706708
AmishaSinghv <84845768+AmishaSinghv@users.noreply.github.com>
2021-05-27 20:41:46 +0530

    Create navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import Dropdown from './Dropdown';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const closeMobileMenu=()=> setClick(false);
+    const showButton = () => {
+        if(window.innerWidth<=960)
+        {
+            setButton(false);
+        }
+        else
+        {
+            setButton(true);
+        }
+    };
+  return (
+  <>
+      <nav className='navbar'>
+        <Link to='/' className='navbar-logo' >
+          EPIC
+          <i class='fab fa-firstdraft' />
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          
+        </div>
+        </nav>
+        );
+        }
+       
+          
+           
+
+export default Navbar;
