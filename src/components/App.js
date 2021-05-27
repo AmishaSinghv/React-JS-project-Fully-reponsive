fd024d377aec48a25c5da31cdc3080cb0584c461
AmishaSinghv <84845768+AmishaSinghv@users.noreply.github.com>
2021-05-27 20:53:44 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+       
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
